a57472c150cc1a0f68238400b5154f508495ad9d
shooooting <shooootingsta@gmail.com>
2020-11-21 00:44:18 +0900

    로그인 error 창 만들기

--- InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
+++ InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
@@ -181,6 +181,31 @@ class LoginViewController: UIViewController {
             return
         }
         
+        // login functionality
+        var username: String?
+        var email: String?
+        
+        if usernameEmail.contains("@"), usernameEmail.contains(".") {
+            // email
+            email = usernameEmail
+        } else {
+            // username
+            username = usernameEmail
+        }
+        
+        AuthManager.shared.loginUser(username: username, email: email, password: password) { success in
+            DispatchQueue.main.async {
+                if success {
+                    // user logged in
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    // error occurred
+                    let alert = UIAlertController(title: "Log In Error", message: "We were unable to log you in.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                    self.present(alert, animated: true)
+                }
+            }
+        }
     }
     
     @objc
--- InstagramClone/Resource/AuthManager.swift
+++ InstagramClone/Resource/AuthManager.swift
@@ -18,7 +18,20 @@ public class AuthManager {
         
     }
     
-    public func loginUser(username: String, email: String, password: String) {
-        
+    public func loginUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void) {
+        if let email = email {
+            // email log in
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                guard authResult != nil, error == nil else {
+                    completion(false)
+                    return
+                }
+                completion(true)
+            }
+            
+        } else if let username = username {
+            // username log in
+            print(username)
+        }
     }
 }
